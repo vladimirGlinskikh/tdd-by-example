54dfa188876ffdc9f2ef85b33ccaa3a0283b2c09
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-12-17 07:48:28 +0600

    Equality for all

--- src/test/java/kz/zhelezyaka/MoneyTest.java
+++ src/test/java/kz/zhelezyaka/MoneyTest.java
@@ -3,6 +3,7 @@ package kz.zhelezyaka;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class MoneyTest {
     @Test
@@ -14,5 +15,11 @@ public class MoneyTest {
         assertEquals(15, product.amount);
         product = coin.times(4);
         assertEquals(20, product.amount);
+    }
+
+    @Test
+    void testEquality() {
+        assertEquals(new Dollar(5), new Dollar(5));
+        assertNotEquals(new Dollar(5), new Dollar(7));
     }
 }
