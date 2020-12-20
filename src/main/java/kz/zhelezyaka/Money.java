554ff5a945b7d831ddf01c2167456afc60891d0a
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-12-20 23:21:05 +0600

    Makin' Objects

--- src/main/java/kz/zhelezyaka/Money.java
+++ src/main/java/kz/zhelezyaka/Money.java
@@ -1,7 +1,17 @@
 package kz.zhelezyaka;
 
-public class Money {
+public abstract class Money {
     protected int amount;
+
+    public abstract Money times(int multiplier);
+
+    public static Money dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    public static Money franc(int amount) {
+        return new Franc(amount);
+    }
 
     public boolean equals(Object object) {
         Money money = (Money) object;
