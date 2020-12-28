9e538fbd5f461702da86776e56b1fdc6822f2134
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2020-12-28 22:35:44 +0600

    Mixed currencies

--- src/main/java/kz/zhelezyaka/Expression.java
+++ src/main/java/kz/zhelezyaka/Expression.java
@@ -2,4 +2,5 @@ package kz.zhelezyaka;
 
 public interface Expression {
     Money reduce(Bank bank, String to);
+    Expression plus(Expression addend);
 }
--- src/main/java/kz/zhelezyaka/Money.java
+++ src/main/java/kz/zhelezyaka/Money.java
@@ -21,18 +21,16 @@ public class Money implements Expression {
         return new Money(amount, "CHF");
     }
 
-    public Money times(int multiplier) {
+    public Expression times(int multiplier) {
         return new Money(amount * multiplier, this.currency);
     }
 
-    public Expression plus(Money addend) {
+    public Expression plus(Expression addend) {
         return new Sum(this, addend);
     }
 
     @Override
     public Money reduce(Bank bank, String to) {
-//        return this;
-//        int rate = (currency.equals("CHF") && to.equals("USD")) ? 2 : 1;
         return new Money(amount / bank.rate(this.currency, to), to);
     }
 
