fd5c4f2b1b5ce05dc05e0bb752c0396990a84261
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2021-01-01 21:51:18 +0600

    Trick question

--- src/main/java/kz/zhelezyaka/javaRush/App.java
+++ src/main/java/kz/zhelezyaka/javaRush/App.java
@@ -2,10 +2,13 @@ package kz.zhelezyaka.javaRush;
 
 public class App {
     public static void main(String[] args) {
-        double middleElement = 11;
-        System.out.println((int)Math.ceil(middleElement/2));
-        System.out.println("------------------");
-        double middleElement1 = 11;
-        System.out.println(middleElement1/2+1);
+//        double middleElement = 11;
+//        System.out.println((int)Math.ceil(middleElement/2));
+//        System.out.println("------------------");
+//        double middleElement1 = 11;
+//        System.out.println(middleElement1/2+1);
+
+        char[] array = new char[-0];
+        System.out.println(array);
     }
 }
